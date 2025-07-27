1b34e5bc9a436071339b1b5b163c683e7f247193
gelv9 <gabriel_elvaz@hotmail.com>
2025-07-27 17:32:53 -0300

    Formulario de registro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login as django_login 
-
+from django.contrib.auth import login as django_login
+from django.contrib.auth import logout as django_logout
+from usuarios.forms import FormularioRegistro
 
 def login(request):
     if request.method == 'POST':
@@ -9,15 +10,22 @@ def login(request):
         if formulario.is_valid():
             user = formulario.get_user()
             django_login(request, user)
-            ...
-            return redirect('inicio')
+            return redirect('inicio')  # <- Asegurate que esta vista exista
     else:
-        formulario = ...()
-        
-    return render(request, 'usuarios/login.html', {'formularios': formulario})
+        formulario = AuthenticationForm()
+
+    return render(request, 'usuarios/login.html', {'formulario': formulario})
 
 def logout(request):
+    django_logout(request)
     return render(request, 'usuarios/logout.html')
 
 def registro(request):
-    return render(request, 'usuarios/registro.html')
+    if request.method == "POST":
+        formulario = FormularioRegistro(request.POST)
+        if formulario.is_valid():
+            formulario.save()
+        return redirect ("ususarios:login")
+    else:
+        formulario = FormularioRegistro()
+    return render(request, 'usuarios/registro.html', {'formulario': formulario})
--- web_cursos/urls.py
+++ web_cursos/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def inicio(request):
+    return HttpResponse('Bienvenido, estas logueado.')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
